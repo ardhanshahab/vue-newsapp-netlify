758fc85ed0cc2b6c786a3b5cd7fee3d77880ba1e
ardhanshahab <ardhanshahab@gmail.com>
2022-05-12 16:40:54 +0700

    testing

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var app = express();
-var port = process.env.PORT || 5000
-app.listen(port);
-console.log('starting project at ' + port);
-app.engine('pug', require('pug').__express);
-app.set('views', '/');
-app.set('view engine', 'pug');
-//Look for statics first
-app.use(serveStatic(path.join(__dirname, '/')));
-//Return the index for any other GET request
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', {root: path.join(__dirname, '/')});
-});
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
